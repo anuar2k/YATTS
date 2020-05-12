e5e3603d053d8ddd32c38c1955264e367f8e4874
anuar2k <anuar2k@outlook.com>
2020-05-12 18:52:22 +0200

    basic TelemVar impl and test

---
+++ YATTS.Plugin/src/scssdk_value_sizes.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "pch.h"
+#include "scs_sdk_1_11/include/scssdk_value.h"
+
+//reflects sizes of types from scssdk_value.h
+const size_t scssdk_value_sizes[] = {
+	0,
+	sizeof(scs_value_bool_t),
+	sizeof(scs_value_s32_t),
+	sizeof(scs_value_u32_t),
+	sizeof(scs_value_u64_t),
+	sizeof(scs_value_float_t),
+	sizeof(scs_value_double_t),
+	sizeof(scs_value_fvector_t),
+	sizeof(scs_value_dvector_t),
+	sizeof(scs_value_euler_t),
+	sizeof(scs_value_fplacement_t),
+	sizeof(scs_value_dplacement_t),
+	sizeof(scs_value_string_t),
+	sizeof(scs_value_s64_t),
+	sizeof(scs_value_s64_t)
+};
