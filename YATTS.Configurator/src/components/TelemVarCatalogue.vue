1ad9ece94361fc7dc0445a92436df26d6444054d
anuar2k <anuar2k@outlook.com>
2020-10-02 12:43:39 +0200

    added search box, applied some new styling

--- YATTS.Configurator/src/components/TelemVarCatalogue.vue
+++ YATTS.Configurator/src/components/TelemVarCatalogue.vue
@@ -1,27 +1,34 @@
 <template>
-  <div class="row disable-selection card bg-3 mb-2" v-for="category in state" v-bind:key="category.def.id">
+  <div class="row card p-2 bg-grey">
     <div class="col">
-      <div class="row" v-on:click="$emit('toggle-show', category)">
-        <div class="col">
-          <span class="category-arrow"><RotatingArrow v-bind:rotated="category.show" /></span>
-          <span class="category-name">{{ category.def.name }}</span>
-          <span class="category-desc">{{ category.def.description }}</span>
-        </div>
+      <div class="row margin-between">
+        <input type="text" id="search" class="card" placeholder="Search for a variable by its name"/>
       </div>
-      <div class="row indent-groups" v-show="category.show">
+      <div class="row disable-selection card margin-between bg-3" v-for="category in state" v-bind:key="category.def.id">
         <div class="col">
-          <div class="row" v-for="group in category.groups" v-bind:key="group.def.id">
+          <div class="row" v-on:click="$emit('toggle-show', category)">
             <div class="col">
-              <div class="row" v-on:click="$emit('toggle-show', group)">
+              <span class="category-arrow"><RotatingArrow v-bind:rotated="category.show" /></span>
+              <span class="category-name">{{ category.def.name }}</span>
+              <span class="category-desc">{{ category.def.description }}</span>
+            </div>
+          </div>
+          <div class="row indent-groups" v-if="category.show">
+            <div class="col">
+              <div class="row" v-for="group in category.groups" v-bind:key="group.def.id">
                 <div class="col">
-                  <RotatingArrow v-bind:rotated="group.show" />&nbsp;{{ group.def.name }}
-                </div>
-              </div>
-              <div class="row indent-variables" v-show="group.show">
-                <div class="col">
-                  <div class="row" v-for="variable in group.variables" v-bind:key="variable.def.id">
-                    <div class="col" v-on:click="$emit('toggle-selected', variable)">
-                      {{ variable.def.id }}, selected: {{ variable.selected }}
+                  <div class="row" v-on:click="$emit('toggle-show', group)">
+                    <div class="col">
+                      <RotatingArrow v-bind:rotated="group.show" />&nbsp;{{ group.def.name }}
+                    </div>
+                  </div>
+                  <div class="row indent-variables" v-if="group.show">
+                    <div class="col">
+                      <div class="row" v-for="variable in group.variables" v-bind:key="variable.def.id">
+                        <div class="col" v-on:click="$emit('toggle-selected', variable)">
+                          {{ variable.def.id }}, selected: {{ variable.selected }}
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -51,6 +58,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .search-card {
+    display: flex;
+    flex-direction: row;
+  }
+
+  #search {
+    font-size: x-large;
+    width: 100%;
+    line-height: 1.5;
+    padding-left: .5rem;
+  }
 
   .indent-groups {
     padding-left: 1.2rem;
