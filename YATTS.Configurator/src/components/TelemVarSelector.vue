609b59bf059779c155ef4950c9021bc358217f5a
anuar2k <anuar2k@outlook.com>
2020-10-09 09:15:05 +0200

    more jsx and card view prototyping

--- YATTS.Configurator/src/components/TelemVarSelector.vue
+++ YATTS.Configurator/src/components/TelemVarSelector.vue
@@ -2,7 +2,7 @@
   <div class="row card p-2 bg-grey">
     <div class="col">
       <div class="row margin-between">
-        <input v-model="query" type="text" id="search" class="card" placeholder="Search for a variable by its name"/>
+        <input v-model="query" type="text" id="search" class="card" placeholder="Search for a variable by its name">
       </div>
       <div class="row disable-selection card margin-between bg-3" v-for="category in filteredConfig" v-bind:key="category.value.def.id">
         <div class="col">
@@ -12,7 +12,7 @@
                 <div class="category-left">
                   <span class="category-arrow"><RotatingArrow v-bind:rotated="category.value.show" /></span>
                   <span class="category-name">{{ category.value.def.name }}</span>
-                  <span class="category-desc">{{ category.value.def.description }}</span>
+                  <span class="category-desc">{{ category.value.def.desc }}</span>
                 </div>
                 <div class="category-right">
                   <span class="category-count">{{ getCategoryVariableCount(category) }}</span>
@@ -40,8 +40,16 @@
                   <div class="row indent-variables" v-if="group.value.show">
                     <div class="col">
                       <div class="row" v-for="variable in group.variables" v-bind:key="variable.value.def.id">
-                        <div class="col" v-on:dblclick="$emit('toggle-selected', variable.value)">
-                          {{ variable.value.def.id }}, selected: {{ variable.value.selected }}
+                        <div class="col">
+                          <div class="variable">
+                            <div class="variable-left">
+                              <input class="variable-selected" type="checkbox" v-model="variable.value.selected">
+                              <span class="variable-name">{{ variable.value.def.name }}</span>
+                            </div>
+                            <div class="variable-right">
+                              <span class="variable-id">{{ variable.value.def.id }}</span>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -66,7 +74,7 @@ import { FilterConfigByVariableName } from '@/utils/TelemVarConfigUtil';
 export default {
   name: 'TelemVarSelector',
   props: ['config'],
-  emits: ['toggle-show', 'toggle-selected'],
+  emits: ['toggle-show'],
   components: {
     RotatingArrow
   },
@@ -105,12 +113,22 @@ export default {
     justify-content: space-between;
   }
 
-  .group-count {
-    font-size: small;
+  .group-arrow {
+    margin-right: .5rem;
   }
 
-  .group-arrow {
-    margin-right: .25rem;
+  .variable {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .variable-id {
+    font-family: 'Courier New', Courier, monospace;
+  }
+
+  .variable-selected {
+    margin-right: .5rem;
   }
 
   .search-card {
@@ -126,11 +144,11 @@ export default {
   }
 
   .indent-groups {
-    padding-left: 1.2rem;
+    padding-left: 1.3rem;
   }
 
   .indent-variables {
-    padding-left: 2rem;
+    padding-left: 1rem;
   }
 
   .disable-selection {
