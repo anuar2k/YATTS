ec346e9cbaf32c06386a969bd5aac75cc5172c66
anuar2k <anuar2k@outlook.com>
2021-03-30 23:07:07 +0200

    favor namespaces over classes with deleted constructor

--- YATTS.Plugin/src/telemvar/FloatConverters.hpp
+++ YATTS.Plugin/src/telemvar/FloatConverters.hpp
@@ -4,11 +4,7 @@
 #include <functional>
 #include <unordered_map>
 
-class FloatConverters {
-	public:
-
-	FloatConverters() = delete;
-
+namespace FloatConverters {
 	enum class Converter {
 		NONE,
 		MS_TO_KMH,
@@ -22,18 +18,17 @@ class FloatConverters {
 		L100KM_TO_MPG
 	};
 
-	static const std::unordered_map<Converter, std::function<double(double)>> converters;
+	const std::unordered_map<Converter, std::function<double(double)>> converters = {
+		{Converter::NONE,          [](double val) { return val * 3.6; }},
+		{Converter::MS_TO_KMH,     [](double val) { return val * 3.6; }},
+		{Converter::MS_TO_MPH,     [](double val) { return val * 2.2369362920544; }},
+		{Converter::PSI_TO_MPA,    [](double val) { return val * 0.006894759086775369; }},
+		{Converter::PSI_TO_BAR,    [](double val) { return val * 0.0689475729; }},
+		{Converter::C_TO_F,        [](double val) { return (val * 9 / 5) + 32; }},
+		{Converter::C_TO_K,        [](double val) { return val + 273.15; }},
+		{Converter::L_TO_GAL,      [](double val) { return val * 0.264172052; }},
+		{Converter::KM_TO_MI,      [](double val) { return val * 0.621371192; }},
+		{Converter::L100KM_TO_MPG, [](double val) { return 235.214583 / val; }}
+	};
 };
 
-const std::unordered_map<FloatConverters::Converter, std::function<double(double)>> FloatConverters::converters = {
-	{Converter::NONE,          [](double val) { return val * 3.6; }},
-	{Converter::MS_TO_KMH,     [](double val) { return val * 3.6; }},
-	{Converter::MS_TO_MPH,     [](double val) { return val * 2.2369362920544; }},
-	{Converter::PSI_TO_MPA,    [](double val) { return val * 0.006894759086775369; }},
-	{Converter::PSI_TO_BAR,    [](double val) { return val * 0.0689475729; }},
-	{Converter::C_TO_F,        [](double val) { return (val * 9 / 5) + 32; }},
-	{Converter::C_TO_K,        [](double val) { return val + 273.15; }},
-	{Converter::L_TO_GAL,      [](double val) { return val * 0.264172052; }},
-	{Converter::KM_TO_MI,      [](double val) { return val * 0.621371192; }},
-	{Converter::L100KM_TO_MPG, [](double val) { return 235.214583 / val; }}
-};
